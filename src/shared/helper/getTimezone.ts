0628fa2a94d9a806af7ccb736f0efaad2fa4f736
om-sv <om-sv2@mail.ru>
2024-03-25 17:53:10 +0500

    timezone fix

--- src/shared/helper/getTimezone.ts
+++ src/shared/helper/getTimezone.ts
@@ -2,12 +2,12 @@ import dayJs from "@/shared/core/dayjs";
 import { timezoneData } from "../core/timezone";
 
 export const getTimezone = (utcId?: string) => {
-  const utcCount = dayJs().utcOffset() / 60;
   if (utcId && utcId?.length !== 0) {
     const userTimezone = timezoneData.find((time) => String(time.id) === utcId);
     return userTimezone;
   } else {
-    const utc = `UTC${utcCount > 0 ? "+" : "-"}${utcCount}`;
+    const utcCount = dayJs(new Date()).utcOffset() / 60;
+    const utc = `UTC${utcCount >= 0 ? "+" : "-"}${utcCount}`;
     console.log(utc);
     const userTimezone = timezoneData.find((time) => time.utc === utc);
     return userTimezone;
